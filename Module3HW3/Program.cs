c6796e9ba6cc65d7d61844c90ad92cd9c6aebe4c
Roman Nikitin <roman.nikitin@internera.systems>
2024-04-30 11:55:00 +0300

    commit

--- Module3HW3/Program.cs
+++ Module3HW3/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var c1 = new Class1();
+            var c2 = new Class2();
+            var result  = c2.Calc(c1.Multiply, 2, 3);
+            var show = new MyShowDelegate(Show);
+            show(result.Invoke(5));
+        }
+        
+        public static void Show(bool b)
+        {
+            if (b)
+            {
+                Console.WriteLine("No reminder after division");
+            } else
+            {
+                Console.WriteLine("There is reminder after division");
+            }
         }
     }
 }
